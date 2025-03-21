ee1a83ba7ed7dbc70d8e838a22a398bab2866055
Alexander Prechtel <aprechtel0043@gmail.com>
2025-03-21 11:54:09 -0400

    User Registration Complete
    Admin ACCOUNT INFORMATION
    
    USER: admin
    EMAIL: admin@example.com
    PASS: admin

--- Final Project Code/ReviewMaster/base/views.py
+++ Final Project Code/ReviewMaster/base/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return render(request, 'base/home.html')
 
+def logoutuser(request):
+    logout(request)
+    return redirect('home')
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
 
-def login(request):
-    return render(request, 'base/login.html')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+    return render(request, 'base/login_register.html')
 
 
-def register(request):
-    return render(request, 'base/register.html')
+def registeruser(request):
+    page = 'register'
+    form = UserCreationForm()
+    context = {
+        'form': form,
+        'page': page
+    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error has occurred during registration')
+    return render(request, 'base/login_register.html', context=context)
 
 
-def review(request, pk):
-    return render(request, 'base/review.html')
+#def review(request, pk):
+#    return render(request, 'base/review.html')
