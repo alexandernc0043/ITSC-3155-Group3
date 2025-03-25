1d60cea1452a4d9958a032e5e4574884bcbb3e46
Diana Barreiro Gonsalve <dbarreir@charlotte.edu>
2025-03-25 13:31:57 -0400

    Update search bar styles

--- Final Project Code/ReviewMaster/static/scripts/main.js
+++ Final Project Code/ReviewMaster/static/scripts/main.js
@@ -1,10 +1,6 @@
 const homeClass = document.getElementsByClassName("home");
 const loginClass = document.getElementsByClassName("login");
 
-if (homeClass.length > 0) {
-    document.getElementsByClassName("header__search")[0].style.display = "none";
-}
-
-if (loginClass.length > 0) {
+if (homeClass.length > 0 || loginClass.length > 0) {
     document.getElementsByClassName("header__search")[0].style.display = "none";
 }
