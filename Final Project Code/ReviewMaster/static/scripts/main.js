de4a159dbe06be6d2d7ef20ef93861e37901a1f4
Diana Barreiro Gonsalve <dbarreir@charlotte.edu>
2025-03-28 00:07:17 -0400

    Added filter for courses and remove search and update styles

--- Final Project Code/ReviewMaster/static/scripts/main.js
+++ Final Project Code/ReviewMaster/static/scripts/main.js
@@ -2,7 +2,7 @@ const homeClass = document.querySelector(".home");
 const loginClass = document.querySelector(".login");
 
 if (homeClass || loginClass) {
-    document.getElementsByClassName("header__search")[0].style.display = "none";
+  document.getElementsByClassName("header__search")[0].style.display = "none";
 }
 
 const dropdownMenu = document.querySelector(".dropdown-menu");
@@ -19,12 +19,33 @@ if (dropdownButton) {
 const alertMessages = document.querySelector(".alert-messages");
 
 if (alertMessages) {
-    const liElement = alertMessages.getElementsByTagName("li");
-    const classList = liElement[0].classList;
-    if (classList.contains("error")) {
-        alertMessages.classList.add("error");
-    }
-    else if (classList.contains("success")) {
-        alertMessages.classList.add("success");
-    }
+  const liElement = alertMessages.getElementsByTagName("li");
+  const classList = liElement[0].classList;
+  if (classList.contains("error")) {
+    alertMessages.classList.add("error");
+  }
+  else if (classList.contains("success")) {
+    alertMessages.classList.add("success");
+  }
 }
+
+const input = document.querySelector(".header__search input");
+const removeInput = document.querySelector(".clear-input-button");
+
+if (input.value) {
+  removeInput.classList.add("show");
+}
+
+removeInput.addEventListener("click", () => {
+  input.value = '';
+  removeInput.classList.remove("show");
+});
+
+input.addEventListener("input", (e) => {
+  e.preventDefault();
+  if (e.target.value && !input.classList.contains("show")) {
+    removeInput.classList.add("show");
+  } else if (!e.target.value && input.classList.contains("show")) {
+    removeInput.classList.remove("show");
+  }
+});
